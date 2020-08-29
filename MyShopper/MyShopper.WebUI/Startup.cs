a63b02f56d0bba992388c8811533d5b02105e404
Samin Momin <saminmomin4@gmail.com>
2020-08-29 12:19:01 +0530

    Initial Solution Setup

---
+++ MyShopper/MyShopper.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShopper.WebUI.Startup))]
+namespace MyShopper.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
