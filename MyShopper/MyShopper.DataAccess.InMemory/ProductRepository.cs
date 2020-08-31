757334c66ec00e8e32daa8565281799efa6771cc
Samin Momin <saminmomin4@gmail.com>
2020-08-31 11:32:38 +0530

    Built Product Management pages and tested them.

--- MyShopper/MyShopper.DataAccess.InMemory/ProductRepository.cs
+++ MyShopper/MyShopper.DataAccess.InMemory/ProductRepository.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace MyShopper.DataAccess.InMemory
 {
-    class ProductRepository
+    public class ProductRepository
     {
         ObjectCache cache = MemoryCache.Default;
         List<Product> products=new List<Product>();
