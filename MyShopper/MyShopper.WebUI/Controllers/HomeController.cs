fb093aac0233d2ad7d256573629adde868746556
Samin Momin <saminmomin4@gmail.com>
2020-09-20 10:30:55 +0530

    Added Product Listings.

--- MyShopper/MyShopper.WebUI/Controllers/HomeController.cs
+++ MyShopper/MyShopper.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyShopper.core;
+using MyShopper.core.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +10,32 @@ namespace MyShopper.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> productContext;
+        IRepository<ProductCategory> productCategoryContext;
+
+        public HomeController(IRepository<Product> productContext,IRepository<ProductCategory> productCategoryContext)
+        {
+            this.productContext = productContext;
+            this.productCategoryContext = productCategoryContext;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = productContext.Collection().ToList();
+            return View(products);
         }
 
+        public ActionResult Details(string id)
+        {
+            Product product = productContext.Find(id);
+            if (product != null)
+            {
+                return View(product);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
