cbfe79745146c05e39114944b722a36d1af743d2
Samin Momin <saminmomin4@gmail.com>
2020-09-23 15:22:24 +0530

    Finished Adding Basket Service

--- MyShopper/MyShopper.Services/BasketService.cs
+++ MyShopper/MyShopper.Services/BasketService.cs
@@ -1,6 +1,7 @@
 ﻿using MyShopper.core;
 using MyShopper.core.Contracts;
 using MyShopper.core.Models;
+using MyShopper.core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,7 @@ using System.Web;
 
 namespace MyShopper.Services
 {
-    public class BasketService
+    public class BasketService:IBasketService
     {
         IRepository<Product> productContext;
         IRepository<Basket> basketContext;
@@ -20,7 +21,7 @@ namespace MyShopper.Services
             this.productContext = Product;
             this.basketContext = Basket;
         }
-        private Basket Get(HttpContext httpContext,bool createIfNull)
+        private Basket Get(HttpContextBase httpContext,bool createIfNull)
         {
             HttpCookie cookie = httpContext.Request.Cookies.Get(BasketSessionName);
             Basket basket = new Basket();
@@ -49,7 +50,7 @@ namespace MyShopper.Services
             return basket;
         }
 
-        private Basket CreateNewBasket(HttpContext httpContext)
+        private Basket CreateNewBasket(HttpContextBase httpContext)
         {
             Basket basket = new Basket();
             basketContext.Insert(basket);
@@ -61,7 +62,7 @@ namespace MyShopper.Services
             return basket;
         }
 
-        public void AddToBasket(HttpContext httpContext, string productId)
+        public void AddToBasket(HttpContextBase httpContext, string productId)
         {
             Basket basket = Get(httpContext, true);
             BasketItem item = basket.BasketItems.FirstOrDefault(i => i.ProductId == productId);
@@ -77,7 +78,7 @@ namespace MyShopper.Services
             basketContext.Commit();
         }
 
-        public void RemoveFromBasket(HttpContext httpContext,string itemId)
+        public void RemoveFromBasket(HttpContextBase httpContext,string itemId)
         {
             Basket basket = Get(httpContext,true);
             BasketItem item = basket.BasketItems.FirstOrDefault(i => i.Id == itemId);
@@ -85,6 +86,54 @@ namespace MyShopper.Services
             {
                 basket.BasketItems.Remove(item);
                 basketContext.Commit();
+            }
+        }
+
+        public List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext)
+        {
+            //createifnull is set to false as we don't want to create a new basket in case if it doesn't exist
+            Basket basket = Get(httpContext, false);
+            if (basket != null)
+            {
+                var result = (from b in basket.BasketItems
+                              join p in productContext.Collection() on b.ProductId equals p.Id
+                              select new BasketItemViewModel()
+                              {
+                                  Id = b.Id,
+                                  Productname = p.Name,
+                                  Price = p.Price,
+                                  Image = p.Image,
+                                  Quantity = b.quantity
+
+                              }).ToList();
+                return result;
+            }
+            else
+            {
+                return new List<BasketItemViewModel>();
+            }
+
+        }
+
+        public BasketSummaryViewModel GetBasketSummary (HttpContextBase httpContext)
+        {
+            Basket basket = Get(httpContext, false);
+            BasketSummaryViewModel model = new BasketSummaryViewModel(0, 0);
+            if (basket != null)
+            {
+                int? basketcount = (from item in basket.BasketItems
+                                    select item.quantity).Sum();
+                decimal? baskettotal = (from item in basket.BasketItems
+                                        join p in productContext.Collection()
+                                        on item.ProductId equals p.Id
+                                        select item.quantity * p.Price).Sum();
+                model.BasketCount = basketcount??0;
+                model.BasketTotal = baskettotal??decimal.Zero;
+                return model;
+            }
+            else
+            {
+                return model;
             }
         }
     }
---
+++ MyShopper/MyShopper.core/Contracts/IBasketService.cs
@@ -0,0 +1,19 @@
+﻿using MyShopper.core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyShopper.core.Contracts
+{
+    public interface IBasketService
+    {
+        void AddToBasket(HttpContextBase httpContext, string productId);
+        void RemoveFromBasket(HttpContextBase httpContext, string itemId);
+        List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
+        BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+
+    }
+}
