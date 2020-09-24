f1f501310ec5e244eea763738507deaea069184d
Samin Momin <saminmomin4@gmail.com>
2020-09-24 14:04:28 +0530

    Added UI for the Basket

--- MyShopper/MyShopper.WebUI/App_Start/UnityConfig.cs
+++ MyShopper/MyShopper.WebUI/App_Start/UnityConfig.cs
@@ -3,6 +3,7 @@ using MyShopper.core.Contracts;
 using MyShopper.core.Models;
 using MyShopper.DataAccess.InMemory;
 using MyShopper.DataAccess.SQL;
+using MyShopper.Services;
 using System;
 
 using Unity;
@@ -48,6 +49,9 @@ namespace MyShopper.WebUI
             // TODO: Register your type's mappings here.
             container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
             container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Basket>, SQLRepository<Basket>>();
+            container.RegisterType<IRepository<BasketItem>, SQLRepository<BasketItem>>();
+            container.RegisterType<IBasketService, BasketService>();
         }
     }
 }
---
+++ MyShopper/MyShopper.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,43 @@
+﻿using MyShopper.core.ViewModels;
+using MyShopper.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShopper.WebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        public BasketService basketService;
+        public BasketController(BasketService BasketService)
+        {
+            this.basketService = BasketService;
+        }
+        // GET: Basket
+        public ActionResult Index()
+        {
+            var model = basketService.GetBasketItems(this.HttpContext);
+            return View(model);
+        }
+        
+        public ActionResult AddToBasket(string Id)
+        {
+            basketService.AddToBasket(this.HttpContext, Id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveFromBasket(string Id)
+        {
+            basketService.RemoveFromBasket(this.HttpContext, Id);
+            return RedirectToAction("Index");
+        }
+
+        public PartialViewResult GetBasketSummary()
+        {
+            var model = basketService.GetBasketSummary(this.HttpContext);
+            return PartialView(model);
+        }
+    }
+}
