67dff35456bc99ac7998125716a20c941d959e97
Samin Momin <saminmomin4@gmail.com>
2020-10-02 11:45:04 +0530

    Linked Customers and Orders

--- MyShopper/MyShopper.WebUI/Controllers/BasketController.cs
+++ MyShopper/MyShopper.WebUI/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
-﻿using MyShopper.core.Models;
+﻿using MyShopper.core.Contracts;
+using MyShopper.core.Models;
 using MyShopper.core.ViewModels;
 using MyShopper.Services;
 using System;
@@ -11,12 +12,14 @@ namespace MyShopper.WebUI.Controllers
 {
     public class BasketController : Controller
     {
-        public BasketService basketService;
-        public OrderService orderService;
-        public BasketController(BasketService BasketService,OrderService orderService)
+        public IBasketService basketService;
+        public IOrderService orderService;
+        public IRepository<Customer> customers;
+        public BasketController(IBasketService BasketService,IOrderService orderService, IRepository<Customer> customers)
         {
             this.basketService = BasketService;
             this.orderService = orderService;
+            this.customers = customers;
         }
         // GET: Basket
         public ActionResult Index()
@@ -42,17 +45,36 @@ namespace MyShopper.WebUI.Controllers
             var model = basketService.GetBasketSummary(this.HttpContext);
             return PartialView(model);
         }
-
+        [Authorize]
         public ActionResult CheckOut()
         {
-            return View();
+            Customer customer = customers.Collection().FirstOrDefault(c => c.Email == User.Identity.Name);
+            if (customer != null)
+            {
+                Order order = new Order()
+                {
+                    FirstName = customer.FirstName,
+                    LastName = customer.LastName,
+                    City = customer.City,
+                    Street = customer.Street,
+                    State = customer.State,
+                    ZipCode = customer.ZipCode,
+                    Email = customer.Email
+                };
+                return View(order);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
         }
         [HttpPost]
+        [Authorize]
         public ActionResult CheckOut(Order order)
         {
             var basketItems = basketService.GetBasketItems(this.HttpContext);
             order.OrderStatus = "Order Created";
-
+            order.Email = User.Identity.Name;
             //process payment
 
             order.OrderStatus = "Payment Processed";
